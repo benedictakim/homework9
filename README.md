f75b6fc5733fd980e585c0e2da6dde877dbb13f8
Benn <61118590+benedictakim@users.noreply.github.com>
2020-08-08 21:00:36 -0400

    modified:   README.md modified:   utils/generateMarkdown.js -fixed readme, all working

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# pt
 
+# Project Title : abc title
+## Project Description:
+def description
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Test](#test)
+* [Questions](#questions)
+* [License](#license)
+* [Author] (#Author)
+* [Badges](#badges)
+## Installation
+ghi inst
+## Usage
+jkl usage
+## Contributors
+mno
+## Test
+pqr
+## Questions
+If you have any questions, contact stuv at wxyz@email.com on GitHub.
+## License
+MIT License
+## Author 
+!Benedicta Kim
+## Badges
+!(https://img.shields.io/github/repo-size/benedictakim/homework9)
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,31 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `
+# Project Title : ${data.title}
+## Project Description:
+${data.projectdescription}
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Test](#test)
+* [Questions](#questions)
+* [Badges](#badges)
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## License
+${data.license}
+## Contributors
+${data.contributing}
+## Test
+${data.test}
+## Questions
+If you have any questions, contact ${data.githubname} at ${data.email} on GitHub.
+## Badges
+![badmath](https://img.shields.io/github/repo-size/${data.username}/${data.repo})
 `;
 }
 
